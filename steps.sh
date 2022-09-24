ff103246d0bb22c935499c63bb7b4c980d9ee7da
phantomic12 <yoavtirosh54@gmail.com>
2022-09-24 02:51:25 -0400

    add back noconfirm

--- steps.sh
+++ steps.sh
@@ -67,7 +67,7 @@ pacman -S --needed --noconfirm archiso mkinitcpio-archiso
 pacman-key --init
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 }
 
 # Copy ezreleng to working directory
