52938ca1157079e66d9befebde83cea9f534c0c7
phantomic12 <yoavtirosh54@gmail.com>
2022-09-24 02:50:17 -0400

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -64,6 +64,7 @@ sleep 2
 prepreqs () {
 pacman -S --noconfirm archlinux-keyring
 pacman -S --needed --noconfirm archiso mkinitcpio-archiso
+pacman-key --init
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
