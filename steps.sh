d3bf05f66d3d67487210261a3164405b7fb06e36
phantomic12 <yoavtirosh54@gmail.com>
2022-10-03 15:57:05 -0400

    pull custom additions from main

--- steps.sh
+++ steps.sh
@@ -64,7 +64,11 @@ sleep 2
 prepreqs () {
 pacman -S --noconfirm archlinux-keyring
 pacman -S --needed --noconfirm archiso mkinitcpio-archiso
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 }
+
 
 # Copy ezreleng to working directory
 cpezreleng () {
