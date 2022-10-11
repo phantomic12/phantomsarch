14a6c576a7b8d413f22d5464ca14edd7abdfa298
phantomic12 <yoavtirosh54@gmail.com>
2022-10-11 16:51:59 -0400

    hopefully fix mkarchiso error

--- steps.sh
+++ steps.sh
@@ -8,6 +8,6 @@ pacman-key --lsign-key FBA220DFC880C036
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 
 # mkarchiso run cmd
-mkarchiso -v -w ./work -o ./out
+mkarchiso -v -w ./work -o ./out .
 
       
