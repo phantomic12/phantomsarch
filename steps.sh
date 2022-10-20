1dea43d766682901d11ed476986d981993ea0b12
phantomic12 <yoavtirosh54@gmail.com>
2022-10-20 16:07:45 -0400

    enable libvirtd

--- steps.sh
+++ steps.sh
@@ -8,6 +8,11 @@ pacman-key --lsign-key FBA220DFC880C036
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 pacman -Sy
 
+
+# enable systemd services
+mkdir -p airootfs/etc/systemd/system/sockets.target.wants
+ln -s /usr/lib/systemd/system/libvirtd.service airootfs/etc/systemd/system/sockets.target.wants
+
 # mkarchiso run cmd
 mkarchiso -v -w ./ -o ./out .
 
