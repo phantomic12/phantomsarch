52f826837cd2aff90bac65d321127326cebed9a0
phantomic12 <yoavtirosh54@gmail.com>
2022-10-21 22:13:03 -0400

    replace gnome with cinn

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="phantoms-arch"
-iso_label="phantoms-arch"
-iso_publisher="phantomic12 <https> "
-iso_application="phantomsarch live cd"
+iso_name="archlinux-gui-cinnamon"
+iso_label="ARCH_GUI_CINNAMON_$(date +%Y%m)"
+iso_publisher="DemonKiller <https://archlinuxgui.in>"
+iso_application="Arch Linux GUI Live/Rescue CD"
 iso_version="$(date +%Y.%m)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
@@ -19,4 +19,7 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/alg-remove-nvidia"]="0:0:755"
+  ["/usr/local/bin/alg-preset"]="0:0:755"
+  ["/usr/local/bin/alg-finalisation"]="0:0:755"
 )
