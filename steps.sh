d0fbe1b8fd03e5575d43392b8ff25cf49f8828f0
phantomic12 <76127596+phantomic12@users.noreply.github.com>
2022-12-02 15:49:34 -0500

    ironed out the last of the errors hopefully

--- steps.sh
+++ steps.sh
@@ -2,12 +2,6 @@
 pacman -Sy
 pacman -S --noconfirm archlinux-keyring
 pacman -S --needed --noconfirm archiso mkinitcpio-archiso
-pacman-key --init
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
-pacman -Sy
-
 
 # enable systemd services
 mkdir -p airootfs/etc/systemd/system/sockets.target.wants
