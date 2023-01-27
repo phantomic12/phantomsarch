a073f3123546ec8652290e24f659ac9073a4259f
phantomic12 <76127596+phantomic12@users.noreply.github.com>
2023-01-27 12:16:26 -0500

    hope this fixes symlink hell

--- repo/clone.sh
+++ repo/clone.sh
@@ -1,71 +1,71 @@
 mkdir PKGBUILDS && cd PKGBUILDS && rm -rf *
-git clone https://aur.archlinux.org/brave-nightly-bin.git
-git clone https://aur.archlinux.org/microsoft-edge-dev-bin.git
-git clone https://aur.archlinux.org/discord-canary.git
-git clone https://aur.archlinux.org/python-proton-client.git
-git clone https://aur.archlinux.org/fvs.git
-git clone https://aur.archlinux.org/icoextract.git
-git clone https://aur.archlinux.org/patool.git
-git clone https://aur.archlinux.org/vkbasalt-cli.git
-git clone https://aur.archlinux.org/mozilla-common.git
-git clone https://aur.archlinux.org/playonlinux.git
-git clone https://aur.archlinux.org/visual-studio-code-bin.git
-git clone https://aur.archlinux.org/webstorm-eap.git
-git clone https://aur.archlinux.org/jetbrains-fleet.git
-git clone https://aur.archlinux.org/pycharm-edu.git
-git clone https://aur.archlinux.org/pycharm-professional.git
-git clone https://aur.archlinux.org/noisetorch.git
-git clone https://aur.archlinux.org/appimagelauncher.git
-git clone https://aur.archlinux.org/pulsar-bin.git
-git clone https://aur.archlinux.org/waterfox-g4-bin.git
-git clone https://aur.archlinux.org/ventoy-bin.git
-git clone https://aur.archlinux.org/waterfox-g-bin.git
-git clone https://aur.archlinux.org/pycharm-community-jre.git
-git clone https://aur.archlinux.org/slack-desktop.git
-git clone https://aur.archlinux.org/heroic-games-launcher-bin.git
-git clone https://aur.archlinux.org/android-studio.git
-git clone https://aur.archlinux.org/anydesk-bin.git
-git clone https://aur.archlinux.org/inxi.git
-git clone https://aur.archlinux.org/heroku-cli-bin.git
-git clone https://aur.archlinux.org/microsoft-edge-stable-bin.git
-git clone https://aur.archlinux.org/python2-bin.git
-git clone https://aur.archlinux.org/yay-bin.git
-git clone https://aur.archlinux.org/gnome-shell-extension-espresso.git
-git clone https://aur.archlinux.org/gnome-shell-extension-gsconnect.git
-git clone https://aur.archlinux.org/gnome-shell-extension-dash-to-dock.git
-git clone https://aur.archlinux.org/gnome-shell-extension-clipboard-indicator.git
-git clone https://aur.archlinux.org/gnome-shell-extension-nasa-apod.git
-git clone https://aur.archlinux.org/gnome-shell-extension-openweather.git
-git clone https://aur.archlinux.org/gnome-shell-extension-blur-my-shell.git
-git clone https://aur.archlinux.org/gnome-shell-extension-no-overview.git
-git clone https://aur.archlinux.org/gnome-shell-extension-dash-to-panel.git
-git clone https://aur.archlinux.org/gnome-shell-extension-media-controls.git
-git clone https://aur.archlinux.org/gnome-shell-extension-caffeine.git
-git clone https://aur.archlinux.org/gnome-shell-extension-gtk4-desktop-icons-ng.git
-git clone https://aur.archlinux.org/gnome-shell-extension-ddterm.git
-git clone https://aur.archlinux.org/gnome-shell-extension-just-perfection-desktop.git
-git clone https://aur.archlinux.org/brave-beta-bin.git
-git clone https://aur.archlinux.org/danser.git
-git clone https://aur.archlinux.org/python-protonvpn-nm-lib.git
-git clone https://aur.archlinux.org/protonvpn-gui.git
-git clone https://aur.archlinux.org/python-pynput.git
-git clone https://aur.archlinux.org/etcher-bin.git
-git clone https://aur.archlinux.org/discord-rpc-cli.git
-git clone https://aur.archlinux.org/ungoogled-chromium-bin.git
-git clone https://aur.archlinux.org/rustdesk-bin.git
-git clone https://aur.archlinux.org/snapd.git
-git clone https://aur.archlinux.org/vmware-workstation.git
-git clone https://aur.archlinux.org/vmware-keymaps.git
-git clone https://aur.archlinux.org/vmware-unlocker-bin.git
-git clone https://aur.archlinux.org/microsoft-edge-beta-bin.git
-git clone https://aur.archlinux.org/armcord-bin.git
-git clone https://aur.archlinux.org/octopi.git
-git clone https://aur.archlinux.org/jellyfin-media-player.git
-git clone https://aur.archlinux.org/gdlauncher-bin.git
-git clone https://aur.archlinux.org/tlauncher-org.git
-git clone https://aur.archlinux.org/lunar-client.git
-git clone https://aur.archlinux.org/virtualbox-ext-oracle.git
-git clone https://aur.archlinux.org/vscodium-bin.git
-git clone https://aur.archlinux.org/jdownloader2.git
-git clone https://aur.archlinux.org/betterdiscord-installer.git
-git clone https://aur.archlinux.org/prismlauncher-bin.git
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/brave-nightly-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/microsoft-edge-dev-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/discord-canary.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/python-proton-client.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/fvs.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/icoextract.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/patool.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/vkbasalt-cli.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/mozilla-common.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/playonlinux.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/visual-studio-code-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/webstorm-eap.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/jetbrains-fleet.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/pycharm-edu.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/pycharm-professional.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/noisetorch.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/appimagelauncher.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/pulsar-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/waterfox-g4-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/ventoy-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/waterfox-g-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/pycharm-community-jre.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/slack-desktop.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/heroic-games-launcher-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/android-studio.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/anydesk-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/inxi.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/heroku-cli-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/microsoft-edge-stable-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/python2-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-espresso.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-gsconnect.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-dash-to-dock.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-clipboard-indicator.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-nasa-apod.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-openweather.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-blur-my-shell.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-no-overview.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-dash-to-panel.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-media-controls.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-caffeine.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-gtk4-desktop-icons-ng.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-ddterm.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gnome-shell-extension-just-perfection-desktop.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/brave-beta-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/danser.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/python-protonvpn-nm-lib.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/protonvpn-gui.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/python-pynput.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/etcher-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/discord-rpc-cli.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/ungoogled-chromium-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/rustdesk-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/snapd.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/vmware-workstation.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/vmware-keymaps.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/vmware-unlocker-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/microsoft-edge-beta-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/armcord-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/octopi.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/jellyfin-media-player.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/gdlauncher-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/tlauncher-org.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/lunar-client.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/virtualbox-ext-oracle.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/vscodium-bin.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/jdownloader2.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/betterdiscord-installer.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/prismlauncher-bin.tar.gz
